1be73e6d80a59074448416fbd91605caaaf094dd
Ronnie Rios <ronniesmail14@gmail.com>
2023-07-25 11:31:20 -0500

    initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import './App.css'
+import Home from './pages/Home';
+import Nav from './components/Nav';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/' element={Home}/>
+        </Routes>
+      </Router>
     </>
   )
 }
