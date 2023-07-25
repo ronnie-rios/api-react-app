a3caa0e20c39aaea3482673358fc1f109715cb6d
Ronnie Rios <ronniesmail14@gmail.com>
2023-07-25 15:40:54 -0500

    fixed error handler

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+    
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -21,13 +21,13 @@ const Home = () => {
     fetchData();
   }, []);
 
-  if (loading) return <p>loading</p>;
+  if (loading) return <p className="p-10">loading</p>;
 
-  if(error) return <p className="text-2xl text-red-600">Sorry there was an error</p>
 
   return (
     <main className="p-10">
-      <h1 className="p-4">Posts</h1>
+      <h1 className="text-2xl text-strong pb-10">Posts</h1>
+      {error ? <p className="text-2xl text-red-600">Sorry there was an error: {error.message}</p> : ''}
       {posts.map((post) => (
         <Card key={post.id} post={post} />
       ))}
