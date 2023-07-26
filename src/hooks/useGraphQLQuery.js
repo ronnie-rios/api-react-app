400b336c51721c1f68ddbeacac09b6f8ce03f4e2
Ronnie Rios <ronniesmail14@gmail.com>
2023-07-25 19:21:25 -0500

    custom hook

--- src/hooks/useGraphQLQuery.js
+++ src/hooks/useGraphQLQuery.js
@@ -1,2 +1,35 @@
 import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+
+const useGraphQLQuery = (query, variables, id) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const { data: queryData, loading: queryLoading, error: queryError } = useQuery(query, {
+        variables: variables, id
+      });
+    
+      useEffect(() => {
+        if (queryLoading) {
+          setLoading(true);
+        } else {
+          setLoading(false);
+        }
+    
+        if (queryError) {
+          setError(queryError);
+        } else {
+          setError(null);
+        }
+    
+        if (queryData) {
+          setData(queryData);
+        }
+      }, [queryData, queryLoading, queryError]);
+    
+      return { data, loading, error };
+};
+
+export default useGraphQLQuery;
+
