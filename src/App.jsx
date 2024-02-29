dcdbf0f368bb5ae0f8ada02460604bf695511207
Ronnie Rios <ronniemarkrios@gmail.com>
2024-02-28 19:22:52 -0600

    crud

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Details from "./pages/postPages/Details";
 import AllMovies from "./pages/moviePages/AllMovies";
 import MovieDetails from "./pages/moviePages/MovieDetails";
 import Example from "./components/Example";
+import Crud from "./pages/prac/Crud";
 import { CartProvider } from "./store/cartStore";
 import Cart from "./components/Cart";
 
@@ -21,6 +22,7 @@ function App() {
             <Route path="/movies/:id" element={<MovieDetails />} />
             <Route path="/example" element={<Example />} />
             <Route path="/test" element={<Cart />} />
+            <Route path="/crud" element={<Crud />} />
           </Routes>
         </CartProvider>
       </Router>
